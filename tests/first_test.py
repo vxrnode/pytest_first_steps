40a9be608bbb194465ea1c308c8db3c9cb03baa2
vxrnode <vxr.node@gmail.com>
2022-05-17 12:02:41 +0300

    Add file with positive tests

---
+++ tests/first_test.py
@@ -0,0 +1,26 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calulate_correctly(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_subtract_calulate_correctly(self):
+        assert self.calc.subtraction(self, 4, 2) == 2
+
+    def test_adding_calulate_correctly(self):
+        assert self.calc.adding(self, 2, 2) == 4
+
+ # def test_multipyl_calculation_failed(self):
+ #     assert self.calc.multiply(self, 2, 2) == 5
+
+
+
+
