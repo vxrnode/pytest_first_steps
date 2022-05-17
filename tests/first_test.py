2579d4c6bbfbe3e53a73ec9bba7b4f399b63bd17
vxrnode <vxr.node@gmail.com>
2022-05-17 12:21:07 +0300

    Add file with positive tests

--- tests/first_test.py
+++ tests/first_test.py
@@ -18,9 +18,9 @@ class TestCalc:
     def test_adding_calulate_correctly(self):
         assert self.calc.adding(self, 2, 2) == 4
 
- # def test_multipyl_calculation_failed(self):
- #     assert self.calc.multiply(self, 2, 2) == 5
-
+# def test_multipyl_calculation_failed(self):
+#     assert self.calc.multiply(self, 2, 2) == 5
+# Add to Git
 
 
 
